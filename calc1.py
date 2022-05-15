3a37c69a5507e45a74b6d06d5129396536f4898b
ryo0705s <inspire.sasaki15@gmail.com>
2022-05-15 17:18:01 +0900

    顧客情報を２次元配列で格納

--- calc1.py
+++ calc1.py
@@ -7,4 +7,8 @@ file = "WORK_分析共有_顧客情報"
 csv_file = open(master + dir + file + ".csv", encoding="shift-jis")
 c = csv.reader(csv_file)
 header = next(c)
-print(header)
+c_list = []
+for i in c:
+  c_list.append(i)
+  
+print(c_list) 
